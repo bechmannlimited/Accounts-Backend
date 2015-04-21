79f865ef85d9c40772530e02ab0272d865e066ed
a1exb1 <alex_bechmann@hotmail.com>
2015-04-21 14:25:27 +0200

    can now send direct payments and add purchases to autogenerate transactions

--- Accounts_IOU/Accounts_IOU/Controllers/TransactionController.cs
+++ Accounts_IOU/Accounts_IOU/Controllers/TransactionController.cs
@@ -11,12 +11,59 @@ namespace Accounts_IOU.Controllers
         // GET: Payment
         public JsonResult AddTransaction(Transaction transaction)
         {
+            transaction.TransactionDate = DateTime.Now; // TODO: get from instance
             transaction.DateEntered = DateTime.Now;
-
+            transaction.PurchaseID = 0;
+            transaction.Description = "";
+            
             db.Transactions.Add(transaction);
             db.SaveChanges();
 
             return Json(transaction);
+        }
+
+        public JsonResult AddPurchase(Purchase purchase, int[] relationUserIDs, double[] relationUserAmounts)
+        {
+            ResponseStatus status = ResponseStatus.Failed;
+            string message = "";
+
+            try
+            {
+                purchase.DatePurchased = DateTime.Now; // TODO: get from instance
+                purchase.DateEntered = DateTime.Now;
+
+                db.Purchases.Add(purchase);
+                db.SaveChanges();
+
+                for (int i = 0; i < relationUserIDs.Length; i++)
+			    {
+                    int relationUserID = relationUserIDs[i];
+                    double amount = relationUserAmounts[i];
+
+			        Transaction transaction = new Transaction();
+                    transaction.UserID = (int)purchase.UserID;
+                    transaction.RelationUserID = relationUserID;
+                    transaction.Amount = amount;
+                    db.Transactions.Add(transaction);
+			    }
+
+                db.SaveChanges();
+                status = ResponseStatus.Success;
+            }
+            catch (Exception e)
+            {
+                message = e.Message;
+                
+            }
+
+            var response = new ResponseDictionary(status);
+
+            if (message.Length > 0)
+            {
+                response["Message"] = message;
+            }
+
+            return Json(response, JsonRequestBehavior.AllowGet);
         }
 
         public JsonResult DifferenceBetweenUsers(User u, long relationUserID)
